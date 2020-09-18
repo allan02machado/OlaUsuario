92fffbdbdbf307fefd4e9d283b89927bd66365f0
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-18 13:24:13 -0300

    Este commit adiciona resposta ao exercício

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OlaUsuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o seu nome: ");
+            string Allan = Console.ReadLine();
+            Console.Write("Olá, ");
+            Console.Write("Allan");
+            Console.Write("!");
+        }
+    }
+}
